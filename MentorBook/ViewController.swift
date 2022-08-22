c3da47256838789f562836ebcfc60789cad4fd1f
HiromasaHori <horihiromasa.tech@gmail.com>
2022-08-22 22:24:24 +0900

    Initial Commit

---
+++ MentorBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MentorBook
+//
+//  Created by 堀弘昌 on 2022/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
